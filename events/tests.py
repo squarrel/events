f6b9d861036235e426ebc768277cbe9284e9cbcd
Milos <milosbulatovic3@gmail.com>
2020-12-19 10:00:10 +0100

    test event creation

--- events/tests.py
+++ events/tests.py
@@ -1,3 +1,14 @@
+from datetime import datetime
 from django.test import TestCase
+from events.models import Event
 
-# Create your tests here.
+
+class TestEvent(TestCase):
+
+    def test_creation(self):
+        date_time = datetime(2021, 1, 5, 20, 0, 0)
+        date_time = date_time.strftime('%Y-%m-%d %H:%M:%S')
+        event = Event.objects.create(name='AC/DC Concert', date_time=date_time)
+
+        events = Event.objects.all()
+        self.assertEqual(len(events), 1)
