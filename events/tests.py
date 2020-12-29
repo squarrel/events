9a8e87e48a2bc4d7fd9d5211b207731c64d44421
Milos <milosbulatovic3@gmail.com>
2020-12-29 11:49:13 +0100

    rename test

--- events/tests.py
+++ events/tests.py
@@ -5,7 +5,7 @@ from events.models import Event
 
 class TestEvent(TestCase):
 
-    def test_creation(self):
+    def test_event_create(self):
         date_time = datetime(2021, 1, 5, 20, 0, 0)
         date_time = date_time.strftime('%Y-%m-%d %H:%M:%S')
         event = Event.objects.create(name='AC/DC Concert', date_time=date_time)
