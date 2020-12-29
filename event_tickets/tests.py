b65606537eac9c16b17a733557003f585cedf53b
Milos <milosbulatovic3@gmail.com>
2020-12-29 11:48:29 +0100

    add basic test

--- event_tickets/tests.py
+++ event_tickets/tests.py
@@ -1,3 +1,19 @@
+from datetime import datetime
 from django.test import TestCase
+from events.models import Event
+from event_tickets.models import Ticket, TicketType, Reservation
 
-# Create your tests here.
+
+class TestTicket(TestCase):
+
+    def test_ticket_create(self):
+        date_time = datetime(2021, 1, 5, 20, 0, 0)
+        date_time = date_time.strftime('%Y-%m-%d %H:%M:%S')
+        event = Event.objects.create(name='AC/DC Concert', date_time=date_time)
+
+        tickets_vip = Ticket.objects.create(event=event, ticket_type=TicketType.VIP)
+        tickets_regular = Ticket.objects.create(event=event, ticket_type=TicketType.REGULAR)
+        tickets_premium = Ticket.objects.create(event=event, ticket_type=TicketType.PREMIUM)
+
+        tickets = Ticket.objects.all()
+        self.assertEqual(len(tickets), 3)
