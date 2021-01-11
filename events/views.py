3dcdb22733520f9f34d62283aa172ebc008f0c9d
Milos <milosbulatovic3@gmail.com>
2021-01-11 13:52:17 +0100

    add index view

--- events/views.py
+++ events/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+from django.http import JsonResponse
+from events.models import Event
+from events.serializers import EventSerializer
 
-# Create your views here.
+
+
+@csrf_exempt
+def index(request):
+    if request.method == 'GET':
+        events = Event.objects.all()
+        serializer = EventSerializer(events, many=True)
+        return JsonResponse(serializer.data, safe=False)
