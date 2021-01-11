08575e1bbd47d32bda11d17bd35bdf1ec1b49cc5
Milos <milosbulatovic3@gmail.com>
2021-01-11 07:55:00 +0100

    add reservation to admin

--- event_tickets/admin.py
+++ event_tickets/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
-from event_tickets.models import Ticket
+from event_tickets.models import Reservation, Ticket
 
 
 class TicketAdmin(admin.ModelAdmin):
     pass
 admin.site.register(Ticket, TicketAdmin)
+
+
+class ReservationAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Reservation, ReservationAdmin)
