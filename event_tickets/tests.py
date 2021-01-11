c2ec64565d980260f02efc9735de62d84a3b6f01
Milos <milosbulatovic3@gmail.com>
2021-01-11 13:42:11 +0100

    refactor and add test for buy view

--- event_tickets/tests.py
+++ event_tickets/tests.py
@@ -9,38 +9,31 @@ class TestTicket(TestCase):
     def setUp(self):
         self.client = Client()
 
-    def test_ticket_create(self):
         date_time = datetime(2021, 1, 5, 20, 0, 0)
         date_time = date_time.strftime('%Y-%m-%d %H:%M:%S')
-        event = Event.objects.create(name='AC/DC Concert', date_time=date_time)
+        self.event = Event.objects.create(name='AC/DC Concert', date_time=date_time)
 
-        tickets_vip = Ticket.objects.get(event=event, ticket_type=TicketType.VIP)
-        tickets_regular = Ticket.objects.get(event=event, ticket_type=TicketType.REGULAR)
-        tickets_premium = Ticket.objects.get(event=event, ticket_type=TicketType.PREMIUM)
+        self.tickets_vip = Ticket.objects.get(event=self.event, ticket_type=TicketType.VIP)
+        self.tickets_regular = Ticket.objects.get(event=self.event, ticket_type=TicketType.REGULAR)
+        self.tickets_premium = Ticket.objects.get(event=self.event, ticket_type=TicketType.PREMIUM)
 
+    def test_ticket_create(self):
         tickets = Ticket.objects.all()
         self.assertEqual(len(tickets), 3)
 
     def test_reservation(self):
-        date_time = datetime(2021, 1, 5, 20, 0, 0)
-        date_time = date_time.strftime('%Y-%m-%d %H:%M:%S')
-        event = Event.objects.create(name='AC/DC Concert', date_time=date_time)
-
-        tickets_vip = Ticket.objects.get(event=event, ticket_type=TicketType.VIP)
-        tickets_regular = Ticket.objects.get(event=event, ticket_type=TicketType.REGULAR)
-        tickets_premium = Ticket.objects.get(event=event, ticket_type=TicketType.PREMIUM)
-        tickets_vip.quantity = 300
-        tickets_regular.quantity = 300
-        tickets_premium.quantity = 300
-        tickets_vip.save()
-        tickets_regular.save()
-        tickets_premium.save()
+        self.tickets_vip.quantity = 300
+        self.tickets_regular.quantity = 300
+        self.tickets_premium.quantity = 300
+        self.tickets_vip.save()
+        self.tickets_regular.save()
+        self.tickets_premium.save()
 
         response = self.client.get(
             reverse(
                 'reserve',
                 kwargs = {
-                    'event_id': event.id, 'ticket_type': TicketType.REGULAR,
+                    'event_id': self.event.id, 'ticket_type': TicketType.REGULAR,
                     'first_name': 'Pera', 'last_name': 'Petrovic'
                 }
             )
@@ -48,5 +41,27 @@ class TestTicket(TestCase):
 
         self.assertEqual(200, response.status_code)
 
-        reservations = Reservation.objects.filter(event=event, ticket_type=TicketType.REGULAR)
+        reservations = Reservation.objects.filter(event=self.event, ticket_type=TicketType.REGULAR)
         self.assertEqual(len(reservations), 1)
+
+    def test_buy_without_reservation(self):
+        self.tickets_vip.quantity = 300
+        self.tickets_regular.quantity = 300
+        self.tickets_premium.quantity = 300
+        self.tickets_vip.save()
+        self.tickets_regular.save()
+        self.tickets_premium.save()
+
+        response = self.client.get(
+            reverse(
+                'buy',
+                kwargs = {
+                    'event_id': self.event.id, 'ticket_type': TicketType.PREMIUM
+                }
+            )
+        )
+
+        self.assertEqual(200, response.status_code)
+
+        tickets_premium = Ticket.objects.get(event=self.event, ticket_type=TicketType.PREMIUM)
+        self.assertEqual(299, tickets_premium.quantity)
