03a6442bd0f4d2cab6051318ac6515971669ea7a
Milos <milosbulatovic3@gmail.com>
2021-01-11 10:25:45 +0100

    add reservation serializer

--- event_tickets/serializers.py
+++ event_tickets/serializers.py
@@ -7,3 +7,10 @@ class TicketSerializer(serializers.ModelSerializer):
     class Meta:
         model = Ticket
         fields = ['event', 'ticket_type', 'quantity']
+
+
+class ReservationSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Reservation
+        fields = ['event', 'ticket_type', 'first_name', 'last_name', 'start_time']
