6a4acc0919d295b8fd306be604b4f696a0740a34
Milos <milosbulatovic3@gmail.com>
2021-01-11 10:26:08 +0100

    add reserve url

--- event_tickets/urls.py
+++ event_tickets/urls.py
@@ -5,5 +5,6 @@ from event_tickets import views
 
 urlpatterns = [
     path('', views.index),
-    url(r'^available/(?P<event_id>\d+)$', views.available)
+    url(r'^available/(?P<event_id>\d+)$', views.available),
+    url(r'^reserve/(?P<event_id>\d+)/(?P<ticket_type>\d+)/(?P<first_name>\w+)/(?P<last_name>\w+)/$', views.reserve),
 ]
