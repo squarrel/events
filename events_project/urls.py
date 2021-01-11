5a387e72b846bc076f71bbf75452a2f20258502f
Milos <milosbulatovic3@gmail.com>
2021-01-11 07:57:37 +0100

    include app urls

--- events_project/urls.py
+++ events_project/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('tickets/', include('event_tickets.urls')),
+    path('events/', include('events.urls')),
 ]
